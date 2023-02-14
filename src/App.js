3bf82ea64b9ee96b2592c29e18278455ab5dcdad
Gabriel <gabrielnovatoo@gmail.com>
2023-02-14 20:07:02 -0300

    Add font-family poppins and adjust scrollbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       </header>
       <main>
         <Home />
+        <About />
       </main>
     </div>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import './About.css';
+
+const About = () => {
+    return (
+        <p>teste</p>
+    )
+}
+
+
+
+export default About;
