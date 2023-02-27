73aa63ca48e597b85f8df735a5f23d7314d84d63
Gabriel <gabrielnovatoo@gmail.com>
2023-02-27 17:48:39 -0300

    Set download in curriculum.pdf

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,7 +14,7 @@ const About = () => {
                     <h2>Sobre mim</h2>
                     <h3>Florianópolis, Santa Catarina</h3>
                     <p>Me formei em técnico de Telecomunicações pelo IFSC, em 2021. Atualmente estou graduando em Ciência da Computação pela Estácio. Estudo diariamente a parte de front-end desde a metade de 2022, fiz alguns cursos, mas atualmente priorizo o desenvolvimento de projetos, utilizando REACT, SASS, TailwindCSS. </p>
-                    <a href={curriculum}>
+                    <a href={curriculum} download>
                         <div>
                             <img src={download}/>
                             <p>Currículo</p>
