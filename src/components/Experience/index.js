513251b2b685724715a8b41cab7e588d9b288430
Gabriel Silveira <gabrielnovatoo@gmail.com>
2024-09-11 10:32:55 -0300

    ajuste de info

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -22,7 +22,6 @@ const Experience = () => {
                             <p>Durante minha atuação no CIASC (Centro de Informática e Automação do Estado de Santa Catarina), contribui para projetos de alta relevância no âmbito estadual e governamental, especialmente para o DETRAN de Santa Catarina. Entre os principais projetos em que participei estão:</p>
                             <p><strong>DetranNet:</strong> Colaborei no desenvolvimento e manutenção deste ambiente web, que é a principal plataforma de serviços do DETRAN de Santa Catarina, utilizada internamente pelos colaboradores da instituição.</p>
                             <p><strong>Detran Digital:</strong> Participei de pequenas manutenções e implementação de funcionalidades, deste ambiente web voltado ao cidadão, proporcionando maior acessibilidade e eficiência no acesso aos serviços do DETRAN de forma digital e integrada.</p>
-                            <p><strong>Novo Software - Projeto SIT:</strong> Projeto criado com a ideia de melhorar a qualidade de nossos softwares e criar uma maior facilidade de manutenção. Ainda em desenvolvimento. </p>
                             <p>Principais Tecnologias: Java, Spring Boot, Junit, SQL Server, VueJS, Quasar, JIRA.</p>
                         </div>
                     </div>
